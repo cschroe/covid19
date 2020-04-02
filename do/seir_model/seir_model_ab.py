e6b75ceb1a73d9092d0e97043811f4197e3e198e
Chris <schroeder.chris@gmail.com>
2020-04-02 14:01:59 -0600

    no message

--- do/seir_model/seir_model_ab.py
+++ do/seir_model/seir_model_ab.py
@@ -14,45 +14,49 @@ import pylab as pl
 from scipy.integrate import ode, solve_ivp
 
 # Define function
+# S -> E -> I -> R
+#             -> D
 def SEIRModel(d_grid,init,params):
-    N, beta, sigma, gamma, mu = params
-    S, E, I, R = init
+    N, beta, sigma, gamma, mr = params
+    S, E, I, R, D = init
     
     # System of ordinary difference equations
-    dS = mu*(N-S) - (beta*S*I)/N
+    dS = - (beta*S*I)/N
     
-    dE = (beta*S*I)/N - (mu+sigma)*E
+    dE = (beta*S*I)/N - sigma*E
     
-    dI = sigma*E - (mu+gamma)*I
+    dI = sigma*E - gamma*I
     
-    dR = gamma*I - mu*R + mu*N
+    dR = gamma*I
     
-    return(dS, dE, dI, dR)
+    dD = gamma*I*mr
+    
+    return(dS, dE, dI, dR, dD)
     
 # Define SEIR model parameters
 N = 4413146 # Population 
-beta = 0.95 # Infectious rate (probability of transmission per contact)
+beta = 0.15 # Infectious rate (probability of transmission per contact)
 sigma = 1/5.2 # Incubation rate (1/avg. incubation period in days)
-gamma = 1/3 # Recovery rate (1/infectious period in days)
-mu = 0 # Birth rate/death rate
+gamma = 1/18 # Recovery rate (1/avg. duration of illness in days)
+mr = 0.01 # Mortality rate
 
-paramsAB = [N,beta,sigma,gamma,mu]
+paramsAB = [N,beta,sigma,gamma,mr]
 
 # Implied basic reproductive number
-brnr = (sigma/(mu+sigma))*(beta/(mu+gamma))
+Rnot = beta/gamma
 
 # Define initial values
 S = N # Susceptible
-E = 0 # Exposed
-I = 1 # Infected
+E = 300 # Exposed
+I = 300 # Infected
 R = 0 # Recovered
+D = 0 # Deceased
 
-init = [S,E,I,R]
+init = [S,E,I,R,D]
 
 # Define ad-hoc parameters
-hr = 0.14 # Case hospitalization rate
-icur = 0.06 # Case icu rate
-mr = 0.02 # Case mortality rate
+hr = 0.05 # Case hospitalization rate
+icur = 0.01 # Case icu rate
 
 # Define the time grid
 d_min = 0
@@ -63,13 +67,11 @@ d_grid = np.linspace(d_min,d_max,366)
 solution = solve_ivp(fun=lambda t, y:SEIRModel(t,y,paramsAB),t_span=[d_min,d_max],y0=init,t_eval=d_grid)
 
 # Place solution in a pandas data frame
-output = pd.DataFrame({"t":solution["t"],"S":solution["y"][0],"E":solution["y"][1],"I":solution["y"][2],"R":solution["y"][3]}) 
+output = pd.DataFrame({"t":solution["t"],"S":solution["y"][0],"E":solution["y"][1],"I":solution["y"][2],"R":solution["y"][3],"D":solution["y"][4]}) 
 
 # Use ad-hoc parameters to calculate hospitalizations, icu admissions, deaths
 output['hospital'] = output['I']*hr
 output['icu'] = output['I']*icur
-output['new_deaths'] = output['I']*mr
-output['cumu_deaths'] = output['new_deaths'].cumsum()
 
 # Plot
 ## Infected
@@ -79,7 +81,7 @@ hospital_curve = plt.plot("t","hospital","",data=output,color="green",linewidth=
 ## ICU
 icu_curve = plt.plot("t","icu","",data=output,color="black",linewidth=2, label='ICU')
 ## Deaths
-death_curve = plt.plot("t","cumu_deaths","",data=output,color="red",linewidth=2, label='Deaths')
+death_curve = plt.plot("t","D","",data=output,color="red",linewidth=2, label='Deaths')
 ## Environment
 plt.xlabel('Days')
 plt.ylabel('Individuals')
